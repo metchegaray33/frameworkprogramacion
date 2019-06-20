6b5301acbca4a7b252b128340fc918458e751b4f
sebastianflorio <sebastianflorio00@hotmail.com>
2019-06-20 20:21:35 -0300

    implementacion de nueva grilla
    Se implemento el plugin datatables.js. Este plugin permite crear una grilla con todas las funcionalidades necesarias para la gestion de datos. Facil de usar y responsive.

--- js/misRecursos.js
+++ js/misRecursos.js
@@ -56,6 +56,98 @@ $(function() {
        $('#modelId').modal("show");
         
     });
-
+    loadGrid();
       
 });
+
+var jsonRecursos = [
+    {
+        descripcion: "aaaaa", tipo: "bbbb", autor: "pepito", longDescription: "Java es un lenguaje de programación de propósito general, tipado, orientado a objetos, … que permite el desarrollo desde aplicaciones básicas, pasando por aplicaciones empresariales hasta aplicaciones móviles."+
+                                    "Java nacía como un lenguaje de programación que pudiese ser multiplataforma y multidispositivo, bajo el paradigma “Write Once Run Anywhere” (WORA)", imgUrl: "imagenes/java.png" },
+    {
+        descripcion: "aaa3", tipo: "eeee", autor: "pepote", longDescription: "Java es un lenguaje de programación de propósito general, tipado, orientado a objetos, … que permite el desarrollo desde aplicaciones básicas, pasando por aplicaciones empresariales hasta aplicaciones móviles." +
+            "Java nacía como un lenguaje de programación que pudiese ser multiplataforma y multidispositivo, bajo el paradigma “Write Once Run Anywhere” (WORA)", imgUrl: "imagenes/java.png"},
+    {
+        descripcion: "aaa33", tipo: "bbbb", autor: "pepete", longDescription: "Java es un lenguaje de programación de propósito general, tipado, orientado a objetos, … que permite el desarrollo desde aplicaciones básicas, pasando por aplicaciones empresariales hasta aplicaciones móviles." +
+            "Java nacía como un lenguaje de programación que pudiese ser multiplataforma y multidispositivo, bajo el paradigma “Write Once Run Anywhere” (WORA)", imgUrl: "imagenes/java.png"},
+    {
+        descripcion: "aa2", tipo: "uuuuu", autor: "pepon", longDescription: "Java es un lenguaje de programación de propósito general, tipado, orientado a objetos, … que permite el desarrollo desde aplicaciones básicas, pasando por aplicaciones empresariales hasta aplicaciones móviles." +
+            "Java nacía como un lenguaje de programación que pudiese ser multiplataforma y multidispositivo, bajo el paradigma “Write Once Run Anywhere” (WORA)", imgUrl: "imagenes/java.png" }
+];
+
+function populatePreviewRecursos(data) {
+    setPreviewRecursosTitulo(data.titulo);
+    setPreviewRecursosLongDesc(data.longDescription);
+    setPreviewRecursosImg(data.imgUrl);
+}
+
+function setPreviewRecursosTitulo(value) {
+    $("#previewRecursosTitulo").val(value);
+}
+
+function setPreviewRecursosLongDesc(value) {
+    $("#previewRecursosLongDesc").text(value);
+}
+
+function setPreviewRecursosImg(url) {
+    $("#previewRecursosLongDesc").attr("src",url);
+}
+
+
+function loadGrid() {
+
+    var table = $('#tablaRecursos').DataTable({
+        data: jsonRecursos,
+        columns: [
+            { title: "Descripción", data: "descripcion" },
+            { title: "Tipo", data: "tipo" },
+            { title: "Autor", data: "autor" },
+            { title: "buttons", data: "button" },
+        ],
+        "columnDefs": [{
+            "targets": 3,
+            "data": null,
+            "defaultContent": "<button type='button' class= 'btn btn - success btn-sm preview' >Previsualizar</button>"+
+                "<button type='button' class= 'btn btn-warning btn-sm seeUsage' data-toggle='popover' data - trigger='focus'> Ver usos</button>"
+
+                
+                    
+        }]
+    });
+
+
+    $("#tablaRecursos thead").addClass("thead-dark");
+
+    $('#tablaRecursos tbody').on('click', 'button.btn.btn.-.success.btn-sm.preview', function () {
+        var data = table.row($(this).parents('tr')).data();
+        alert(data[0] + "'s preview is: " + data[2]);
+        populatePreviewRecursos(data);
+
+        $("#dialogPreview").dialog({
+        
+            modal: true
+        });
+        //showModalForm(data.titulo, "dialogPreview");
+
+    });
+
+    $('#tablaRecursos tbody').on('click', 'button.btn.btn-warning.btn-sm.seeUsage', function () {
+        var data = table.row($(this).parents('tr')).data();
+        alert(data[0] + "'s usages is: " + data[1]);
+    });
+}
+
+function reloadGrid() {
+    var datatable = $('#tablaRecursos').DataTable();
+    datatable.clear().draw();
+    datatable.rows.add(jsonRecursos); // Add new data
+    datatable.columns.adjust().draw();
+}
+
+
+//function setOnPreviewClickEvent() {
+//    $('#tablaRecursos tbody').on('click', 'button', function () {
+//        var data = table.row($(this).parents('tr')).data();
+//        alert(data[0] + "'s salary is: " + data[5]);
+//    });
+//}
