72bf266391d2e1bf076075dcf4fc4b96d3b76a36
bruno valverde <br1.vmz@gmail.com>
2019-06-20 23:06:54 -0300

    API Rest terminado (post, get, delete, update), también post de archivos

--- model/app.js
+++ model/app.js
@@ -12,7 +12,14 @@ var usuarios_routes = require('./routes/usuarios')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//cors
+//configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //rutas
 app.use('/api', usuarios_routes);
