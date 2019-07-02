30a024547a1e5f27b50c33718277aa8a8874f95f
sebastianflorio <sebastianflorio00@hotmail.com>
2019-07-01 23:36:38 -0300

    js para cursos
    -agregado plugin grilla.
    -grilla se carga desde la base de datos.
    -formulario nuevo curso valida y persiste data en la bd.

---
+++ js/misCursos.js
@@ -0,0 +1,44 @@
+﻿var tableId = "tablaCursos";
+
+$(function () {
+    loadData();
+});
+
+function loadData() {
+    ajaxCall("http://localhost:3700/api/Cursos", null, function (response) {
+        loadGrid(response.cursos);
+    }, undefined, undefined, "GET");
+}
+
+function generateGridButtons() {
+    return  '<button type="button" class="btn btn-warning ml-1 btn-sm ">Editar</button>' +
+        '<button type="button" class="btn btn-primary ml-1 btn-sm">Previsualizar</button>' +
+        '<a href="misUnidades.html">' +
+        '<button type="button" class="btn btn-success ml-1 btn-sm">Adm Unidades</button>' +
+        '</a>' +
+        '<button type="button" class="btn btn-danger ml-1 btn-sm">Borrar</button>';
+}
+
+
+function loadGrid(data) {
+    
+    var table = $('#' + tableId).DataTable({
+        data: data,
+        columns: [
+            { title: "Cursos", data: "nombre_curso" },
+            { title: "Número de unidades", data: "num_unidades" },
+            { title: "introduccion", data: "introduccion", visible: false },
+            { title: "repaso", data: "repaso", visible: false },
+            { title: "activo", data: "activo", visible: false },
+            { title: "id_recurso", data: "id_recurso", visible: false }  
+        ],
+        "columnDefs": [{
+            "targets": 6,
+            "data": null,
+            "defaultContent": generateGridButtons()
+        }]
+    });
+
+    $("#" + tableId + " thead").addClass("thead-dark");
+    
+}
---
+++ js/nuevoCurso.js
@@ -0,0 +1,85 @@
+﻿$(document).ready(function () {
+    cargarControles();
+});
+
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+});
+
+
+var formNuevoCursoId = "formularioNuevoCurso";
+
+function validar() {
+    var form = $("#" + formNuevoCursoId);
+    form.validate({
+        rules: {
+            nombre: {
+                required: true
+            },
+            num_unidades: {
+                required: true
+            },
+            introduccion: {
+                required: true
+            },
+            repaso: {
+                required: true
+            },
+            id_recurso: {
+                required: true
+            }
+        }
+    });
+
+    return form.valid();
+}
+
+
+function cargarControles() {
+    cargarCombos();
+    setBtnCrearOnCliclEvent();
+}
+
+function setBtnCrearOnCliclEvent() {
+    $("#btnCrear").on("click", onCrearClick);
+}
+
+function cargarCombos() {
+    cargarComboRecursos();
+}
+function cargarComboRecursos() {
+    ajaxCall("http://localhost:3700/api/Recursos", null, function (response) {
+
+        var listitems = '';
+
+        for (var i = 0; i < response.recursos.length; i++) {
+            listitems += "<option value='" + response.recursos[i].id + "'>" + response.recursos[i].descripcion + "</option>";
+        }
+
+        $("#cmbRecursos").append(listitems);
+
+    }, undefined, undefined, "GET");
+}
+
+function onCrearClick() {
+    if (validar()) {
+        var item = getSaveItem();
+        item.activo = true;
+        guardar(item);
+    } 
+        
+}
+
+function guardar(item) {
+    var url = "http://localhost:3700/api/save-Cursos";
+
+    ajaxCall(url, JSON.stringify(item), function (response) {
+        alert(response);
+
+    },undefined,undefined,"POST");
+}
+
+function getSaveItem() {
+    return $("#" + formNuevoCursoId).serializeFormJSON();
+}
